65ddb521325fafcd2a6b2837b64f5f7038d44de0
gtpk <gtpk11@gmail.com>
2016-04-08 09:29:47 +0900

    정한이의 캐릭터툴 대신 커밋

---
+++ laughing-pancake_tools/CharacterTool/CreateTool/CreateTool/Character.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateTool
+{
+    class Character
+    {
+        private string imagePath;
+        private string name;
+        private string index;
+        private string power;
+        private string defence;
+        private string speed;
+        private string strength;
+        private string property;
+        public Character()
+        {
+            imagePath = "";
+            name = "";
+            index = "";
+            power = "";
+            defence = "";
+            speed = "";
+            strength = "";
+            property = "";
+        }
+        public Character(string imagePath, string name, string index, string power, string defence, string speed, string strength, string property)
+        {
+            this.imagePath = imagePath;
+            this.name = name;
+            this.index = index;
+            this.power = power;
+            this.defence = defence;
+            this.speed = speed;
+            this.strength = strength;
+            this.property = property;
+        }
+        public string ImagePath
+        {
+            get
+            {
+                return imagePath;
+            }
+            set
+            {
+                imagePath = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Index
+        {
+            get
+            {
+                return index;
+            }
+            set
+            {
+                index = value;
+            }
+        }
+        public string Power
+        {
+            get
+            {
+                return power;
+            }
+            set
+            {
+                power = value;
+            }
+        }
+        public string Defence
+        {
+            get
+            {
+                return defence;
+            }
+            set
+            {
+                defence = value;
+            }
+        }
+        public string Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                speed = value;
+            }
+        }
+        public string Strength
+        {
+            get
+            {
+                return strength;
+            }
+            set
+            {
+                strength = value;
+            }
+        }
+        public string Property
+        {
+            get
+            {
+                return property;
+            }
+            set
+            {
+                property = value;
+            }
+        }
+    }
+}
