286a1a5844ebcdeda5c715244e669467e544b495
GTPK <gtpk11@gmail.com>
2016-05-06 12:21:22 +0900

    빠진내용 추가 커밋

---
+++ laughing-pancake/AutoClasses/MainUI/Node_1.h
@@ -0,0 +1,27 @@
+#ifndef __CCMAINUI_NODE_1_NODE_1_H__
+#define __CCMAINUI_NODE_1_NODE_1_H__
+#include "cocos2d.h"
+#include "ui/CocosGUI.h"
+
+USING_NS_CC;
+using namespace cocos2d;
+using namespace ui;
+
+/// <summary>
+/// 디자이너 지원에 필요한 메서드입니다.
+/// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+/// </summary>
+namespace CCMainUI{
+class Node_1 : public CCObject
+{
+public:
+	Node_1() {}
+public:
+	cocos2d::Sprite * m_deco1_1_1;
+	cocos2d::Sprite * m_deco1_1_0_0;
+	cocos2d::Sprite * m_deco1_1_2_0;
+	cocos2d::Sprite * m_deco1_1_0_1_0;
+cocos2d::Node * root;
+};
+}
+#endif // __CCMAINUI_NODE_1_H__
